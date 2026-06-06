77831815da4269ac0ec05646fccd434e25256ca9
TwilightV <victorwu_386@mail.ustc.edu.cn>
2026-06-06 17:47:55 +0800

    去除除飞书机器人外的更改提交

--- maimai_bot/maimai_bot/plugins/help/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Config(BaseModel):
-    help_config_path: str = "data/help/help.json"
-    help_commands: list[str] = Field(default_factory=lambda: ["help", "帮助"])
-    help_priority: int = 10
-    help_block: bool = True
